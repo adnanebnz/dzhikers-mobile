9397401806168ec3753cc2b06a9ec914d2fc663f
skillz <skillzdev@hotmail.com>
2023-04-09 22:20:19 +0100

    test

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,9 +1,33 @@
-import { View, Text, Image } from "react-native";
+import {
+  StatusBar,
+  Image,
+  FlatList,
+  Dimensions,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import tw from "twrnc";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          "https://dzhikers.up.railway.app/api/users"
+        );
+        setData(result.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <SafeAreaView style={tw`pt-3`}>
       <View style={tw`flex flex-row items-center justify-center gap-2`}>
@@ -18,6 +42,20 @@ export default function Home() {
           Home
         </Text>
       </View>
+      <FlatList>
+        {data.map((item) => {
+          return (
+            <View
+              key={item._id}
+              style={tw` rounded-xl bg-gray-50 flex flex-col px-5 py-3 shadow-md`}
+            >
+              <Text>{item.email}</Text>
+
+              <Text>{item.username}</Text>
+            </View>
+          );
+        })}
+      </FlatList>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
