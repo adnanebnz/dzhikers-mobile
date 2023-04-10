1a3780a4a68dd0a39824a1d44eef8f465768396a
skillz <skillzdev@hotmail.com>
2023-04-10 23:38:26 +0100

    test

--- screens/Randos/Details.js
+++ screens/Randos/Details.js
@@ -6,12 +6,16 @@ import {
   Button,
   ScrollView,
 } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useEffect, useState } from "react";
 import { makeRequest } from "../../makeRequest";
 import tw from "twrnc";
+import moment from "moment/moment";
+import { IconButton, Icon, Center, NativeBaseProvider } from "native-base";
 export default function Details({ route, navigation }) {
   const { id } = route.params;
   const [loading, setLoading] = useState(true);
@@ -33,66 +37,121 @@ export default function Details({ route, navigation }) {
     navigation.goBack();
   };
   return (
-    <SafeAreaView>
-      {loading && (
-        <View
-          style={{
-            display: "flex",
-            marginTop: 300,
-            justifyContent: "center",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <ActivityIndicator size="large" />
-        </View>
-      )}
-      {!loading && (
-        <>
-          <ScrollView
-            ContentContainerstyle={{
+    <NativeBaseProvider>
+      <SafeAreaView style={{ marginTop: 10, paddingHorizontal: 4 }}>
+        {loading && (
+          <View
+            style={{
               display: "flex",
-              flexDirection: "column",
+              marginTop: 300,
+              justifyContent: "center",
+              flexDirection: "row",
               alignItems: "center",
-              marginTop: 20,
             }}
           >
-            <View>
-              <Image
-                source={{ uri: data.img }}
-                style={{
-                  width: 400,
-                  height: 400,
-                  resizeMode: "cover",
-                  zIndex: 0,
-                  borderRadius: 15,
-                }}
-              />
-              <View
-                style={tw`absolute bottom-88 left-2 text-white text-lg font-semibold h-10 w-10 bg-gray-800 rounded-full flex justify-center items-center opacity-80`}
-              >
-                <AntDesign name="arrowleft" size={24} color="white" />
-              </View>
-            </View>
-            <View
-              style={{
+            <ActivityIndicator size="large" />
+          </View>
+        )}
+        {!loading && (
+          <>
+            <ScrollView
+              ContentContainerstyle={{
                 display: "flex",
                 flexDirection: "column",
-                padding: 10,
-                marginTop: 10,
+                alignItems: "center",
               }}
             >
-              <Text style={tw`text-2xl font-bold text-gray-900 mb-3`}>
-                {data.title}
-              </Text>
-              <Text style={tw`text-gray-700`}>{data.desc}</Text>
-              <View style={tw`pt-6`}>
-                <Button title="Réserver" onPress={goBack} />
+              <View>
+                <Image
+                  source={{ uri: data.img }}
+                  style={{
+                    width: "100%",
+                    height: 300,
+                    resizeMode: "cover",
+                    zIndex: 0,
+                    borderRadius: 15,
+                  }}
+                />
+                <View
+                  style={tw`absolute bottom-60 left-2 text-white text-lg font-semibold h-10 w-10 bg-gray-700 rounded-full flex justify-center items-center opacity-75`}
+                >
+                  <IconButton
+                    icon={<Icon as={AntDesign} name="arrowleft" />}
+                    borderRadius="full"
+                    _icon={{
+                      color: "coolGray.50",
+                      size: "md",
+                    }}
+                    _hover={{
+                      bg: "coolGray.800:alpha.20",
+                    }}
+                    _pressed={{
+                      bg: "coolGray.800:alpha.20",
+                      _icon: {
+                        name: "emoji-flirt",
+                      },
+                      _ios: {
+                        _icon: {
+                          size: "2xl",
+                        },
+                      },
+                    }}
+                    _ios={{
+                      _icon: {
+                        size: "2xl",
+                      },
+                    }}
+                  />
+                </View>
               </View>
-            </View>
-          </ScrollView>
-        </>
-      )}
-    </SafeAreaView>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: 10,
+                  marginTop: 10,
+                }}
+              >
+                <View style={tw`mb-3`}>
+                  <Text style={tw`text-2xl font-bold text-gray-900`}>
+                    {data.title}
+                  </Text>
+                  <Text>
+                    de <Text style={tw`text-blue-500`}>{data.organizer}</Text>
+                  </Text>
+                  <View
+                    style={tw`mt-2 bg-gray-200 h-auto w-30 px-2 py-2 rounded-xl flex flex-row gap-1`}
+                  >
+                    <Text style={tw`text-blue-500 text-3xl font-bold`}>
+                      {data.price}
+                    </Text>
+                    <Text style={tw`text-blue-500 text-lg font-bold`}>DZD</Text>
+                  </View>
+                  <View style={tw`mt-1 flex flex-row items-center gap-[2px]`}>
+                    <Ionicons name="ios-time-outline" size={20} color="grey" />
+                    <Text style={tw`text-gray-500`}>
+                      {moment(data.date).format("DD/MM/YYYY")}
+                    </Text>
+                  </View>
+                  <View style={tw`flex flex-row items-center gap[4px]`}>
+                    <MaterialCommunityIcons
+                      name="account-group"
+                      size={20}
+                      color="grey"
+                    />
+                    <Text style={tw`text-gray-500`}>{data.places}</Text>
+                  </View>
+                </View>
+
+                <Text style={tw`text-gray-700`}>{data.desc}</Text>
+                <View style={tw`pt-6`}>
+                  <Button title="Réserver" onPress={goBack} />
+                </View>
+              </View>
+            </ScrollView>
+          </>
+        )}
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
