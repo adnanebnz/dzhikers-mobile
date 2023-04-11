9f998e9f9a2048d1ca646a14c8ac199abb85772d
skillz <skillzdev@hotmail.com>
2023-04-11 01:50:22 +0100

    test

--- screens/Auth/Profile.js
+++ screens/Auth/Profile.js
@@ -1,9 +1,16 @@
-import { View, Text, ActivityIndicator, Image } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import tw from "twrnc";
 import { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Ionicons } from "@expo/vector-icons";
 export default function Profile() {
   const [loading, setLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState(null);
@@ -23,36 +30,40 @@ export default function Profile() {
         </View>
       )}
       {!loading && (
-        <View>
-          {currentUser && (
-            <View style={tw`flex items-center flex-col gap-2`}>
+        <View style={tw`bg-white p-6`}>
+          <View style={tw`flex-row items-center justify-between`}>
+            <View style={tw`flex-row items-center`}>
               <Image
-                source={{ uri: currentUser.details.img }}
-                style={{
-                  width: 100,
-                  height: 100,
-                  borderRadius: 50,
-                  resizeMode: "cover",
+                source={{
+                  uri: currentUser.details.img,
                 }}
+                style={tw`w-12 h-12 rounded-full mr-4`}
               />
-              <Text
-                style={{
-                  ...tw`text-md font-bold text-gray-700`,
-                }}
-              >
-                @{currentUser.details.username}
-              </Text>
-              <Text
-                style={{
-                  ...tw`text-md text-gray-700`,
-                }}
-              >
-                {currentUser.details.lastName} {currentUser.details.firstName}
-              </Text>
-              <Text>{currentUser.details.email}</Text>
-              <Text>{currentUser.details.age}</Text>
+              <View>
+                <Text style={tw`text-lg font-medium`}>
+                  {currentUser.details.lastName} {currentUser.details.firstName}
+                </Text>
+                <Text style={tw`text-gray-500`}>
+                  {currentUser.details.email}
+                </Text>
+              </View>
             </View>
-          )}
+            <TouchableOpacity style={tw`rounded-full bg-gray-200 p-2`}>
+              <Ionicons name="pencil-outline" size={24} color="gray" />
+            </TouchableOpacity>
+          </View>
+          <View style={tw`mt-6`}>
+            <Text style={tw`text-lg font-medium mb-2`}>Informations</Text>
+            <Text style={tw`text-gray-500`}>
+              <Text style={tw`font-bold`}>Age : </Text>
+              {currentUser.details.age}
+            </Text>
+          </View>
+          <View style={tw`mt-5 h-full`}>
+            <Text style={tw`text-lg font-semibold text-center`}>
+              Mes achats et réservations
+            </Text>
+          </View>
         </View>
       )}
     </SafeAreaView>
