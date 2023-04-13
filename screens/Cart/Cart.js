65abc77dae8296e1f3553b8e5bb72831dad12ee3
skillz <skillzdev@hotmail.com>
2023-04-13 22:56:52 +0100

    test

--- screens/Cart/Cart.js
+++ screens/Cart/Cart.js
@@ -2,10 +2,100 @@ import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import tw from "twrnc";
-
+import { ScrollView } from "react-native";
+import {
+  decreaseCount,
+  increaseCount,
+  removeFromCart,
+  setIsCartOpen,
+  clearCart,
+} from "../../state";
+import { useDispatch, useSelector } from "react-redux";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 export default function Cart() {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.price;
+  }, 0);
   return (
-    <SafeAreaView style={tw`pt-3`}>
+    <SafeAreaView style={tw`p-2`}>
+      <ScrollView>
+        {cart.length > 0 ? (
+          <View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={tw`text-xl font-bold`}>
+                Votre panier ({cart.length})
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  dispatch(clearCart());
+                }}
+              >
+                <Ionicons name="close" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                ...tw`mt-6`,
+              }}
+            >
+              {cart.map((item) => (
+                <View
+                  key={item._id}
+                  style={{
+                    display: "flex",
+                    backgroundColor: "#fff",
+                    borderRadius: 10,
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    padding: 10,
+                    borderBottomWidth: 1,
+                    borderBottomColor: "#ccc",
+                  }}
+                >
+                  <Image
+                    source={{ uri: item.img3 }}
+                    style={{
+                      width: 75,
+                      height: 75,
+                      resizeMode: "contain",
+                      borderRadius: 4,
+                    }}
+                  />
+                  <Text>{item.title}</Text>
+                  <Text>({item.count})</Text>
+                  <Text>{item.price} DZD</Text>
+                  <TouchableOpacity>
+                    <Ionicons
+                      name="trash-outline"
+                      size={24}
+                      color="red"
+                      onPress={() => {
+                        dispatch(removeFromCart({ _id: item._id }));
+                      }}
+                    />
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </View>
+          </View>
+        ) : (
+          <></>
+        )}
+      </ScrollView>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
--- screens/Shop/Product.js
+++ screens/Shop/Product.js
@@ -2,25 +2,27 @@ import {
   Text,
   ActivityIndicator,
   View,
-  Image,
   Button,
   ScrollView,
   Dimensions,
 } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useEffect, useState } from "react";
 import { makeRequest } from "../../makeRequest";
 import tw from "twrnc";
-import moment from "moment/moment";
 import { IconButton, Icon, NativeBaseProvider } from "native-base";
-
+import { SliderBox } from "react-native-image-slider-box/dist";
+import { useDispatch } from "react-redux";
+import { addToCart } from "./../../state";
 const Product = ({ navigation, route }) => {
+  //TODO LOADER
+
   const { id } = route.params;
   const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   useEffect(() => {
@@ -56,18 +58,42 @@ const Product = ({ navigation, route }) => {
             }}
           >
             <View>
-              <Image
-                source={{ uri: data.img3 }}
-                style={{
-                  width: "100%",
-                  height: 300,
-                  resizeMode: "cover",
-                  zIndex: 0,
-                  borderRadius: 15,
+              <SliderBox
+                images={[data.img, data.img2, data.img3]}
+                sliderBoxHeight={width}
+                dotColor="#1d4ed8"
+                inactiveDotColor="#90A4AE"
+                paginationBoxVerticalPadding={20}
+                resizeMethod={"resize"}
+                resizeMode={"cover"}
+                paginationBoxStyle={{
+                  position: "absolute",
+                  bottom: 0,
+                  padding: 0,
+                  alignItems: "center",
+                  alignSelf: "center",
+                  justifyContent: "center",
+                  paddingVertical: 10,
                 }}
+                dotStyle={{
+                  width: 10,
+                  height: 10,
+                  borderRadius: 5,
+                  marginHorizontal: 0,
+                  padding: 0,
+                  margin: 0,
+                  backgroundColor: "rgba(128, 128, 128, 0.92)",
+                }}
+                ImageComponentStyle={{
+                  borderRadius: 15,
+                  width: "100%",
+                  marginTop: 5,
+                }}
+                imageLoadingColor="#2196F3"
               />
+
               <View
-                style={tw`absolute bottom-60 left-2 text-white text-lg font-semibold h-10 w-10 bg-gray-700 rounded-full flex justify-center items-center opacity-75`}
+                style={tw`absolute bottom-75 left-4 text-white text-lg font-semibold h-10 w-10 bg-gray-700 rounded-full flex justify-center items-center opacity-75`}
               >
                 <IconButton
                   icon={<Icon as={AntDesign} name="arrowleft" />}
@@ -94,7 +120,7 @@ const Product = ({ navigation, route }) => {
                 marginTop: 10,
               }}
             >
-              <View style={tw`mb-3`}>
+              <View style={tw`mb-2`}>
                 <Text style={tw`text-2xl font-bold text-gray-900`}>
                   {data.title}
                 </Text>
@@ -109,28 +135,71 @@ const Product = ({ navigation, route }) => {
                   </Text>
                   <Text style={tw`text-blue-500 text-lg font-bold`}>DZD</Text>
                 </View>
-                <View style={tw`mt-1 flex flex-row items-center gap-[2px]`}>
-                  <Ionicons name="ios-time-outline" size={20} color="grey" />
-                  <Text style={tw`text-gray-500`}>
-                    {moment(data.date).format("DD/MM/YYYY")}
-                  </Text>
-                </View>
-                <View style={tw`flex flex-row items-center gap[4px]`}>
-                  <MaterialCommunityIcons
-                    name="account-group"
-                    size={20}
-                    color="grey"
-                  />
-                  <Text style={tw`text-gray-500`}>{data.places}</Text>
-                </View>
+                {data.quantity > 0 ? (
+                  <View style={tw`mt-1 flex flex-row items-center gap-[2px]`}>
+                    <Text style={tw`text-green-500`}>
+                      En Stock: {data.quantity}
+                    </Text>
+                  </View>
+                ) : (
+                  <View style={tw`mt-1 flex flex-row items-center gap-[2px]`}>
+                    <Text style={tw`text-red-500`}>Rupture de Stock</Text>
+                  </View>
+                )}
               </View>
 
               <Text style={tw`text-gray-700`}>{data.desc}</Text>
-              <View style={tw`pt-6`}>
+              <View style={tw`mt-1`}>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 10,
+                    ...tw`bg-gray-200 px-2 py-2 rounded-xl w-48 mx-auto`,
+                    justifyContent: "center",
+                    marginBottom: 10,
+                  }}
+                >
+                  <Text style={tw`text-gray-700 font-semibold`}>Quantité</Text>
+                  <View
+                    style={{
+                      ...tw`flex flex-row items-center justify-center gap-2
+                      border border-blue-300 rounded-md
+                      `,
+                    }}
+                  >
+                    <IconButton
+                      icon={<Icon as={AntDesign} name="minus" />}
+                      _icon={{
+                        color: "blue.400",
+                      }}
+                      onPress={() => {
+                        setCount(Math.max(count - 1, 1));
+                      }}
+                    />
+                    <Text
+                      style={{
+                        ...tw`text-gray-700 text-lg`,
+                      }}
+                    >
+                      {count}
+                    </Text>
+                    <IconButton
+                      icon={<Icon as={AntDesign} name="plus" />}
+                      _icon={{
+                        color: "blue.400",
+                      }}
+                      onPress={() => {
+                        if (count < data.quantity) setCount(count + 1);
+                      }}
+                    />
+                  </View>
+                </View>
                 <Button
-                  title="Réserver"
+                  title="Ajouter au panier"
                   onPress={() => {
-                    alert("hey");
+                    dispatch(addToCart({ item: { ...data, count } }));
                   }}
                 />
               </View>
