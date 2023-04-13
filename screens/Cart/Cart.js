b5556836ef3c29c5d00331da1ed30585fe4253e0
skillz <skillzdev@hotmail.com>
2023-04-13 23:28:46 +0100

    test

--- screens/Cart/Cart.js
+++ screens/Cart/Cart.js
@@ -12,7 +12,8 @@ import {
 } from "../../state";
 import { useDispatch, useSelector } from "react-redux";
 import { TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { Icon, IconButton, NativeBaseProvider } from "native-base";
 export default function Cart() {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
@@ -21,82 +22,162 @@ export default function Cart() {
     return total + item.count * item.price;
   }, 0);
   return (
-    <SafeAreaView style={tw`p-2`}>
-      <ScrollView>
-        {cart.length > 0 ? (
-          <View>
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Text style={tw`text-xl font-bold`}>
-                Votre panier ({cart.length})
-              </Text>
-              <TouchableOpacity
-                onPress={() => {
-                  dispatch(clearCart());
+    <NativeBaseProvider>
+      <SafeAreaView style={tw`p-2`}>
+        <ScrollView>
+          {cart.length > 0 ? (
+            <View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
               >
-                <Ionicons name="close" size={24} color="black" />
-              </TouchableOpacity>
-            </View>
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                ...tw`mt-6`,
-              }}
-            >
-              {cart.map((item) => (
-                <View
-                  key={item._id}
-                  style={{
-                    display: "flex",
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    padding: 10,
-                    borderBottomWidth: 1,
-                    borderBottomColor: "#ccc",
+                <Text style={tw`text-xl font-bold`}>
+                  Votre panier ({cart.length})
+                </Text>
+                <TouchableOpacity
+                  onPress={() => {
+                    dispatch(clearCart());
                   }}
                 >
-                  <Image
-                    source={{ uri: item.img3 }}
-                    style={{
-                      width: 75,
-                      height: 75,
-                      resizeMode: "contain",
-                      borderRadius: 4,
-                    }}
-                  />
-                  <Text>{item.title}</Text>
-                  <Text>({item.count})</Text>
-                  <Text>{item.price} DZD</Text>
-                  <TouchableOpacity>
-                    <Ionicons
-                      name="trash-outline"
-                      size={24}
-                      color="red"
-                      onPress={() => {
-                        dispatch(removeFromCart({ _id: item._id }));
+                  <Ionicons name="close" size={24} color="black" />
+                </TouchableOpacity>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  ...tw`mt-6`,
+                }}
+              >
+                {cart.map((item) => (
+                  <View>
+                    <View
+                      key={item._id}
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: 10,
+                        ...tw`mb-4 bg-gray-200 p-2 rounded-md`,
                       }}
-                    />
-                  </TouchableOpacity>
-                </View>
-              ))}
+                    >
+                      <Image
+                        source={{ uri: item.img3 }}
+                        style={{
+                          width: 75,
+                          height: 90,
+                          resizeMode: "contain",
+                          borderRadius: 4,
+                        }}
+                      />
+                      <View
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <View
+                          style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: 20,
+                          }}
+                        >
+                          <Text
+                            style={{
+                              ...tw`text-[14px] font-bold`,
+                            }}
+                          >
+                            {item.title} ({item.count})
+                          </Text>
+                          <Text
+                            style={{
+                              ...tw`text-[14px] font-semibold text-gray-800`,
+                            }}
+                          >
+                            {item.price} DZD
+                          </Text>
+                        </View>
+                        <View
+                          style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            gap: 10,
+                            ...tw`mt-5`,
+                          }}
+                        >
+                          <View
+                            style={{
+                              ...tw`flex flex-row items-center justify-center gap-2
+                      border border-blue-300 rounded-md
+                      `,
+                            }}
+                          >
+                            <IconButton
+                              icon={<Icon as={AntDesign} name="minus" />}
+                              _icon={{
+                                color: "blue.400",
+                              }}
+                              onPress={() => {
+                                dispatch(decreaseCount({ _id: item._id }));
+                              }}
+                            />
+                            <Text
+                              style={{
+                                ...tw`text-gray-700 text-lg`,
+                              }}
+                            >
+                              {item.count}
+                            </Text>
+                            <IconButton
+                              icon={<Icon as={AntDesign} name="plus" />}
+                              _icon={{
+                                color: "blue.400",
+                              }}
+                              onPress={() => {
+                                dispatch(increaseCount({ _id: item._id }));
+                              }}
+                            />
+                          </View>
+                          <TouchableOpacity
+                            style={{
+                              alignSelf: "center",
+                            }}
+                          >
+                            <Ionicons
+                              name="trash-outline"
+                              size={28}
+                              color="red"
+                              onPress={() => {
+                                dispatch(removeFromCart({ _id: item._id }));
+                              }}
+                            />
+                          </TouchableOpacity>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                ))}
+              </View>
             </View>
-          </View>
-        ) : (
-          <></>
-        )}
-      </ScrollView>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+          ) : (
+            <>
+              <Text
+                style={{
+                  ...tw`text-center text-xl font-bold mt-10`,
+                }}
+              >
+                Votre panier est vide
+              </Text>
+            </>
+          )}
+        </ScrollView>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
