ebc137648add4e76f2becd620410f74ca9291306
skillz <skillzdev@hotmail.com>
2023-04-14 12:44:04 +0100

    test

--- screens/Auth/Profile.js
+++ screens/Auth/Profile.js
@@ -11,7 +11,6 @@ import { Ionicons } from "@expo/vector-icons";
 import useFetchUser from "../../hooks/useFetchUser";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Button } from "react-native";
 export default function Profile({ navigation }) {
   const { user, userLoading, error } = useFetchUser();
   const [loading, setLoading] = useState(true);
@@ -47,36 +46,76 @@ export default function Profile({ navigation }) {
                   <Text style={tw`text-gray-500 text-[12px]`}>
                     {user.details.email}
                   </Text>
+                  <Text style={tw`text-gray-500 text-[12px]`}>
+                    {user.details.age} ans
+                  </Text>
                 </View>
               </View>
               <TouchableOpacity style={tw`rounded-full bg-gray-200 p-2`}>
                 <Ionicons name="pencil-outline" size={24} color="gray" />
               </TouchableOpacity>
             </View>
-            <View style={tw`mt-6`}>
-              <Text style={tw`text-lg font-medium mb-2`}>Informations</Text>
-              <Text style={tw`text-gray-500`}>
-                <Text style={tw`font-bold`}>Age : </Text>
-                {user.details.age}
-              </Text>
-            </View>
-            <View style={tw`mt-20 h-full flex flex-col gap-5`}>
-              <Button
-                title="Voir mes reservations"
+
+            <View style={tw`mt-10 h-full flex flex-col gap-5`}>
+              <TouchableOpacity
                 onPress={() =>
                   navigation.navigate("BookingListing", {
                     id: user.details._id,
                   })
                 }
-              />
-              <Button
-                title="Voir mes achats"
+              >
+                <View>
+                  <Image
+                    source={require("../../assets/hikers.jpg")}
+                    style={{
+                      width: "100%",
+                      height: 180,
+                      opacity: 0.8,
+                      shadowColor: "#000",
+                      resizeMode: "cover",
+                      zIndex: 0,
+                      borderRadius: 15,
+                    }}
+                  />
+                  <Text
+                    style={tw`text-white text-2xl font-bold
+                  absolute bottom-0 p-3
+                  `}
+                  >
+                    Mes réservations
+                  </Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity
                 onPress={() =>
                   navigation.navigate("PurchasedProducts", {
                     id: user.details._id,
                   })
                 }
-              />
+              >
+                <View>
+                  <Image
+                    source={require("../../assets/camping.jpg")}
+                    style={{
+                      width: "100%",
+                      height: 180,
+                      opacity: 0.8,
+                      shadowColor: "#000",
+                      resizeMode: "cover",
+                      zIndex: 0,
+                      borderRadius: 15,
+                    }}
+                  />
+                  <Text
+                    style={tw`text-white text-2xl font-bold
+                  absolute bottom-0 p-3
+                  `}
+                  >
+                    Mes achats
+                  </Text>
+                </View>
+              </TouchableOpacity>
             </View>
           </View>
         </View>
