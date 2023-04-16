7d166165146912703c4375dc0cda19e4507eafe4
skillz <skillzdev@hotmail.com>
2023-04-16 19:20:50 +0100

    test

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -10,20 +10,32 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Menu, Divider, Provider, Badge } from "react-native-paper";
-import useFetchUser from "../../hooks/useFetchUser";
 import { makeRequest } from "../../makeRequest";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { ActivityIndicator } from "react-native";
 
 export default function Home({ navigation }) {
   const [visible, setVisible] = useState(false);
-  const { user, userLoading, error } = useFetchUser();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const closeMenu = () => setVisible(false);
   const [searchText, setSearchText] = useState("");
-  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", async () => {
+      try {
+        const res = await AsyncStorage.getItem("currentUser");
+        setUser(JSON.parse(res));
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
   const handleClear = () => {
     setSearchText("");
   };
@@ -40,206 +52,224 @@ export default function Home({ navigation }) {
   };
   return (
     <Provider>
-      <Menu
-        visible={visible}
-        onDismiss={closeMenu}
-        anchor={{
-          x: 350,
-          y: 80,
-          width: 0,
-          height: 0,
-        }}
-        anchorPosition="top right"
-      >
-        <Menu.Item
-          onPress={() => {
-            navigation.navigate("Profile");
+      {loading ? (
+        <View
+          style={{
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-          leadingIcon={() => (
-            <Ionicons name="person" size={24} color="#6b7280" />
-          )}
-          title="Profile"
-        />
-        <Divider />
-        <Menu.Item
-          onPress={handleDisconnect}
-          leadingIcon={() => (
-            <Ionicons name="log-out" size={24} color="#6b7280" />
-          )}
-          title="Deconnecter"
-        />
-      </Menu>
-
-      <SafeAreaView style={tw`pt-4`}>
-        <View style={tw`flex flex-row items-center justify-between`}>
-          <View style={tw`flex flex-row items-center gap-1 px-4`}>
-            <Text style={tw` text-xl text-black font-black`}>DZHIKERS</Text>
-
-            <Image
-              source={require("./../../assets/noback.png")}
-              style={{ width: 60, height: 60, resizeMode: "contain" }}
-            />
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row-reverse",
-              alignItems: "center",
-              gap: 5,
+        >
+          <ActivityIndicator size="large" />
+        </View>
+      ) : (
+        <>
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={{
+              x: 350,
+              y: 80,
+              width: 0,
+              height: 0,
             }}
+            anchorPosition="top right"
           >
-            {!user && (
-              <TouchableOpacity
-                style={tw`px-2`}
-                onPress={() => {
-                  navigation.navigate("Login");
-                }}
-              >
-                <Image
-                  source={require("../../assets/noavatar.png")}
-                  style={styles.avatar}
-                />
-              </TouchableOpacity>
-            )}
-            {user && (
-              <TouchableOpacity
-                style={tw`px-2`}
-                onPress={() => {
-                  setVisible(true);
-                }}
-              >
-                <Image
-                  source={{ uri: user.details.img }}
-                  style={styles.avatar}
-                />
-              </TouchableOpacity>
-            )}
-            <TouchableOpacity
-              style={tw`px-2`}
+            <Menu.Item
               onPress={() => {
-                navigation.navigate("Panier");
+                navigation.navigate("Profile");
               }}
-            >
+              leadingIcon={() => (
+                <Ionicons name="person" size={24} color="#6b7280" />
+              )}
+              title="Profile"
+            />
+            <Divider />
+            <Menu.Item
+              onPress={handleDisconnect}
+              leadingIcon={() => (
+                <Ionicons name="log-out" size={24} color="#6b7280" />
+              )}
+              title="Deconnecter"
+            />
+          </Menu>
+
+          <SafeAreaView style={tw`pt-4`}>
+            <View style={tw`flex flex-row items-center justify-between`}>
+              <View style={tw`flex flex-row items-center gap-1 px-4`}>
+                <Text style={tw` text-xl text-black font-black`}>DZHIKERS</Text>
+
+                <Image
+                  source={require("./../../assets/noback.png")}
+                  style={{ width: 60, height: 60, resizeMode: "contain" }}
+                />
+              </View>
               <View
                 style={{
-                  position: "absolute",
-                  top: -13,
-                  right: -5,
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                  alignItems: "center",
+                  gap: 5,
                 }}
               >
-                {cart.length > 0 && <Badge>{cart.length}</Badge>}
+                {!user && (
+                  <TouchableOpacity
+                    style={tw`px-2`}
+                    onPress={() => {
+                      navigation.navigate("Login");
+                    }}
+                  >
+                    <Image
+                      source={require("../../assets/noavatar.png")}
+                      style={styles.avatar}
+                    />
+                  </TouchableOpacity>
+                )}
+                {user && (
+                  <TouchableOpacity
+                    style={tw`px-2`}
+                    onPress={() => {
+                      setVisible(true);
+                    }}
+                  >
+                    <Image
+                      source={{ uri: user.details.img }}
+                      style={styles.avatar}
+                    />
+                  </TouchableOpacity>
+                )}
+                <TouchableOpacity
+                  style={tw`px-2`}
+                  onPress={() => {
+                    navigation.navigate("Panier");
+                  }}
+                >
+                  <View
+                    style={{
+                      position: "absolute",
+                      top: -13,
+                      right: -5,
+                    }}
+                  >
+                    {cart.length > 0 && <Badge>{cart.length}</Badge>}
+                  </View>
+                  <MaterialCommunityIcons
+                    name="shopping-outline"
+                    size={24}
+                    color="black"
+                  />
+                </TouchableOpacity>
               </View>
-              <MaterialCommunityIcons
-                name="shopping-outline"
-                size={24}
-                color="black"
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={tw`mt-1 px-3`}>
-          <View>
-            <Text style={tw`text-2xl`}>
-              Explorez <Text style={tw`font-bold`}>l'Algérie</Text> avec nous!
-            </Text>
-          </View>
-          <View style={styles.container}>
-            <Ionicons
-              name="search"
-              size={24}
-              color="gray"
-              style={styles.icon}
-            />
-            <TextInput
-              style={styles.input}
-              value={searchText}
-              onChangeText={setSearchText}
-              placeholder="Rechercher"
-            />
-            {searchText !== "" && (
-              <TouchableOpacity onPress={handleClear}>
+            </View>
+            <View style={tw`mt-1 px-3`}>
+              <View>
+                <Text style={tw`text-2xl`}>
+                  Explorez <Text style={tw`font-bold`}>l'Algérie</Text> avec
+                  nous!
+                </Text>
+              </View>
+              <View style={styles.container}>
                 <Ionicons
-                  name="close"
+                  name="search"
                   size={24}
                   color="gray"
                   style={styles.icon}
                 />
-              </TouchableOpacity>
-            )}
-          </View>
-        </View>
-
-        <View style={tw`mt-3`}>
-          <Text style={tw`font-semibold text-xl px-4`}>Nous offrons</Text>
-          <View style={tw`flex flex-row gap-3 pt-2 px-2`}>
-            <View style={tw`rounded-full bg-blue-500 px-4 py-2`}>
-              <Text style={tw`text-lg text-white`}>Des randonées</Text>
+                <TextInput
+                  style={styles.input}
+                  value={searchText}
+                  onChangeText={setSearchText}
+                  placeholder="Rechercher"
+                />
+                {searchText !== "" && (
+                  <TouchableOpacity onPress={handleClear}>
+                    <Ionicons
+                      name="close"
+                      size={24}
+                      color="gray"
+                      style={styles.icon}
+                    />
+                  </TouchableOpacity>
+                )}
+              </View>
             </View>
-            <View style={tw`rounded-full bg-blue-500 px-4 py-2`}>
-              <Text style={tw`text-lg text-white`}>Differents produits</Text>
+
+            <View style={tw`mt-3`}>
+              <Text style={tw`font-semibold text-xl px-4`}>Nous offrons</Text>
+              <View style={tw`flex flex-row gap-3 pt-2 px-2`}>
+                <View style={tw`rounded-full bg-blue-500 px-4 py-2`}>
+                  <Text style={tw`text-lg text-white`}>Des randonées</Text>
+                </View>
+                <View style={tw`rounded-full bg-blue-500 px-4 py-2`}>
+                  <Text style={tw`text-lg text-white`}>
+                    Differents produits
+                  </Text>
+                </View>
+              </View>
             </View>
-          </View>
-        </View>
 
-        <View style={tw`flex flex-row items-center justify-center mt-3`}>
-          <Text style={tw`text-[16px] text-gray-600`}>
-            Parcourrez et choisissez ce qui vous convient!
-          </Text>
-        </View>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          style={tw`pt-6 mx-[9px] `}
-          contentContainerStyle={tw`flex flex-row items-center justify-center gap-2`}
-        >
-          <View>
-            <Image
-              source={require("./../../assets/hikers.jpg")}
-              style={styles.image}
-            />
+            <View style={tw`flex flex-row items-center justify-center mt-3`}>
+              <Text style={tw`text-[16px] text-gray-600`}>
+                Parcourrez et choisissez ce qui vous convient!
+              </Text>
+            </View>
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              style={tw`pt-6 mx-[9px] `}
+              contentContainerStyle={tw`flex flex-row items-center justify-center gap-2`}
+            >
+              <View>
+                <Image
+                  source={require("./../../assets/hikers.jpg")}
+                  style={styles.image}
+                />
 
-            <Text
-              style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
-            >
-              Randonées
-            </Text>
-          </View>
+                <Text
+                  style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
+                >
+                  Randonées
+                </Text>
+              </View>
 
-          <View>
-            <Image
-              source={require("./../../assets/chair.jpg")}
-              style={styles.image}
-            />
-            <Text
-              style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
-            >
-              Chaises
-            </Text>
-          </View>
-          <View>
-            <Image
-              source={require("./../../assets/tante.jpg")}
-              style={styles.image}
-            />
-            <Text
-              style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
-            >
-              Tantes
-            </Text>
-          </View>
-          <View style={tw``}>
-            <Image
-              source={require("./../../assets/clothes.jpg")}
-              style={styles.image}
-            />
-            <Text
-              style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
-            >
-              Vetements
-            </Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+              <View>
+                <Image
+                  source={require("./../../assets/chair.jpg")}
+                  style={styles.image}
+                />
+                <Text
+                  style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
+                >
+                  Chaises
+                </Text>
+              </View>
+              <View>
+                <Image
+                  source={require("./../../assets/tante.jpg")}
+                  style={styles.image}
+                />
+                <Text
+                  style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
+                >
+                  Tantes
+                </Text>
+              </View>
+              <View style={tw``}>
+                <Image
+                  source={require("./../../assets/photo.png")}
+                  style={styles.image}
+                />
+                <Text
+                  style={tw`absolute bottom-2 left-2 text-white text-lg font-semibold`}
+                >
+                  Vetements
+                </Text>
+              </View>
+            </ScrollView>
+          </SafeAreaView>
+        </>
+      )}
     </Provider>
   );
 }
